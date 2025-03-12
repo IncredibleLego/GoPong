39d0431e394ede9e5635028436c7e3a15ab9a541
IncredibleLego <ec.francesco@gmail.com>
2025-03-12 22:59:25 +0100

    first running window

--- main.go
+++ main.go
@@ -13,8 +13,46 @@ const (
 	paddleSpeed  = 6
 )
 
+type Ojbect struct {
+	X, Y, W, H int // X, Y, Width, Height. (0,0) is top-left.
+}
+
+type Paddle struct {
+	Ojbect
+}
+
+type Ball struct {
+	Ojbect
+	dxdt int // x velocity per tick
+	dydt int // y velocity per tick
+}
+
+type Game struct {
+	paddle    Paddle
+	ball      Ball
+	score     int
+	highScore int
+}
+
 func main() {
-	if err := ebiten.RunGame(NewGame()); err != nil {
+	ebiten.SetWindowTitle("Pong in Go")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	g := &Game{}
+
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Update() error {
+	return nil
 }
