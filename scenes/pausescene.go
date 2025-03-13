82ce8c77b73f345027a55e0ea75957a737f6c206
IncredibleLego <ec.francesco@gmail.com>
2025-03-13 12:35:39 +0100

    improved pause scene

--- scenes/pausescene.go
+++ scenes/pausescene.go
@@ -1,11 +1,12 @@
 package scenes
 
 import (
-	"image/color"
+	"bytes"
+	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
 type PauseScene struct {
@@ -18,10 +19,42 @@ func NewPauseScene() *PauseScene {
 	}
 }
 
+func (g *PauseScene) Draw(screen *ebiten.Image) {
+	// Text Options
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(pressStart2P))
+	if err != nil {
+		log.Fatal(err)
+	}
+	pressStart2PFaceSource = s
+
+	scoreTextOptions := &text.DrawOptions{}
+	scoreTextOptions.GeoM.Translate(250, 180)
+	scoreTextOptions.ColorScale.Scale(1, 1, 1, 1)
+	scoreTextOptions.LineSpacing = 1.5
+
+	highScoreTextOptions := &text.DrawOptions{}
+	highScoreTextOptions.GeoM.Translate(180, 200)
+	highScoreTextOptions.ColorScale.Scale(1, 1, 1, 1)
+	highScoreTextOptions.LineSpacing = 1.5
+
+	textFace := &text.GoTextFace{
+		Source: pressStart2PFaceSource,
+		Size:   13,
+	}
+
+	scoreStr := "Pause menu"
+	text.Draw(screen, scoreStr, textFace, scoreTextOptions)
+
+	HighScoreStr := "Press Enter to unpause"
+	text.Draw(screen, HighScoreStr, textFace, highScoreTextOptions)
+
+}
+
+/*
 func (s *PauseScene) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0, 255, 0, 255})
 	ebitenutil.DebugPrint(screen, "Press enter to unpause.")
-}
+} */
 
 func (s *PauseScene) FirstLoad() {
 	s.loaded = true
