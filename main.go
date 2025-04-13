8a43bf8144fd8a6e21bcef6373c93659bbc5d3d3
IncredibleLego <ec.francesco@gmail.com>
2025-04-14 00:03:03 +0200

    inverted beginning configuration order

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
+	config.InitConfig()
+	//fmt.Printf("Loaded configuration: %+v\n", config.GlobalConfig)
+
 	ebiten.SetWindowTitle("Pong in Go")
 	ebiten.SetWindowSize(config.GlobalConfig.ScreenWidth, config.GlobalConfig.ScreenHeight)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-
-	config.InitConfig()
-	//fmt.Printf("Loaded configuration: %+v\n", config.GlobalConfig)
 
 	game := NewGame()
 
