f16e4f93f09d57ee670ec59a4671f5a2f829ace2
IncredibleLego <ec.francesco@gmail.com>
2025-04-28 13:13:58 +0200

    modified title draw and normalized RGBA values in utils.Color

--- utils/utils.go
+++ utils/utils.go
@@ -63,45 +63,65 @@ func XCentered(message string, fontSize float64) float64 {
 func Color(colorName string) (float32, float32, float32, float32) {
 	switch colorName {
 	case "white":
-		return 255, 255, 255, 255
+		// RGBA: 255, 255, 255, 255
+		return 1, 1, 1, 1
 	case "black":
-		return 0, 0, 0, 255
+		// RGBA: 0, 0, 0, 255
+		return 0, 0, 0, 1
 	case "red":
-		return 255, 0, 0, 255
+		// RGBA: 255, 0, 0, 255
+		return 1, 0, 0, 1
 	case "green":
-		return 0, 255, 0, 255
+		// RGBA: 0, 255, 0, 255
+		return 0, 1, 0, 1
 	case "blue":
-		return 0, 0, 255, 255
+		// RGBA: 0, 0, 255, 255
+		return 0, 0, 1, 1
 	case "yellow":
-		return 255, 255, 0, 255
+		// RGBA: 255, 255, 0, 255
+		return 1, 1, 0, 1
 	case "cyan":
-		return 0, 255, 255, 255
+		// RGBA: 0, 255, 255, 255
+		return 0, 1, 1, 1
 	case "magenta":
-		return 255, 0, 255, 255
+		// RGBA: 255, 0, 255, 255
+		return 1, 0, 1, 1
 	case "light gray":
-		return 204, 204, 204, 255
+		// RGBA: 204, 204, 204, 255
+		return 0.8, 0.8, 0.8, 1
 	case "dark gray":
-		return 51, 51, 51, 255
+		// RGBA: 51, 51, 51, 255
+		return 0.2, 0.2, 0.2, 1
 	case "orange":
-		return 255, 128, 0, 255
+		// RGBA: 255, 128, 0, 255
+		return 1, 0.5, 0, 1
 	case "pink":
-		return 255, 128, 179, 255
+		// RGBA: 255, 128, 179, 255
+		return 1, 0.5, 0.7, 1
 	case "lime":
-		return 128, 255, 0, 255
+		// RGBA: 128, 255, 0, 255
+		return 0.5, 1, 0, 1
 	case "sky blue":
-		return 77, 153, 255, 255
+		// RGBA: 77, 153, 255, 255
+		return 0.3, 0.6, 1, 1
 	case "purple":
-		return 153, 0, 255, 255
+		// RGBA: 153, 0, 255, 255
+		return 0.6, 0, 1, 1
 	case "brown":
-		return 153, 77, 0, 255
+		// RGBA: 153, 77, 0, 255
+		return 0.6, 0.3, 0, 1
 	case "dark red":
-		return 128, 0, 0, 255
+		// RGBA: 128, 0, 0, 255
+		return 0.5, 0, 0, 1
 	case "dark green":
-		return 0, 128, 0, 255
+		// RGBA: 0, 128, 0, 255
+		return 0, 0.5, 0, 1
 	case "dark blue":
-		return 0, 0, 128, 255
+		// RGBA: 0, 0, 128, 255
+		return 0, 0, 0.5, 1
 	case "dark purple":
-		return 102, 0, 153, 255
+		// RGBA: 102, 0, 153, 255
+		return 0.4, 0, 0.6, 1
 	default:
 		log.Printf("Unknown color: %s", colorName)
 		return 0, 0, 0, 255 // Default to black
