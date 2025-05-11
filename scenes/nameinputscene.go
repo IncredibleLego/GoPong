82d5f60a2b39f61d83749d6a8d6f4355f19c0b2f
IncredibleLego <ec.francesco@gmail.com>
2025-05-11 03:06:58 +0200

    fully implemented and created input.go to handle keyboard inputs, improved heavily nameinputscene.go

--- scenes/nameinputscene.go
+++ scenes/nameinputscene.go
@@ -19,7 +19,6 @@ type NameInputScene struct {
 	maxLetters   int
 	maxLenght    bool
 	timer        time.Time
-	lastMoveTime time.Time
 }
 
 func NewNameInputScene(mode int) *NameInputScene {
@@ -44,30 +43,22 @@ func (n *NameInputScene) Draw(screen *ebiten.Image) {
 		return
 	}
 
+	if time.Since(n.timer) > time.Second*2 {
+		n.timer = time.Now()
+	}
 	l := float64(len(n.playerNames[n.activePlayer]))
-
 	height := float64(config.GlobalConfig.ScreenHeight)
-
 	d := 20 - config.GlobalConfig.TextDimension
 
 	playerMessage := "Player " + strconv.Itoa(n.activePlayer+1) + ", insert your name:"
 	x1 := utils.XCentered(playerMessage, config.GlobalConfig.TextDimension)
-
 	utils.ScreenDraw(d, x1, height/3, "yellow", screen, playerMessage)
 
+	message := n.playerNames[n.activePlayer]
 	if time.Since(n.timer) < time.Second && !n.maxLenght {
-		utils.ScreenDraw(d, float64(config.GlobalConfig.ScreenWidth)/2-(l*20/2), height/2, "white", screen, n.playerNames[n.activePlayer]+"_")
-	} else {
-		utils.ScreenDraw(d, float64(config.GlobalConfig.ScreenWidth)/2-(l*20/2), height/2, "white", screen, n.playerNames[n.activePlayer])
+		message += "_"
 	}
-
-	if time.Since(n.timer) > time.Second*2 {
-		n.timer = time.Now()
-	}
-
-	//utils.ScreenDraw(d, float64(config.GlobalConfig.ScreenWidth)/2-(l*20/2), height/2, "white", screen, n.playerNames[n.activePlayer])
-
-	//utils.ScreenDraw(d, 130, height/3*2, "white", screen, "Press Enter to confirm")
+	utils.ScreenDraw(d, float64(config.GlobalConfig.ScreenWidth)/2-(l*20/2), height/2, "white", screen, message)
 
 	confirmMessage := "Press Enter to confirm"
 	x2 := utils.XCentered(confirmMessage, config.GlobalConfig.TextDimension)
@@ -102,59 +93,10 @@ func (n *NameInputScene) Update() SceneId {
 		return GameSceneId
 	}
 
-	moveInterval := time.Duration(time.Second / 8)
+	utils.Input(&n.playerNames[n.activePlayer], n.maxLetters)
 
-	// Check if max letters has been reached
-	if len(n.playerNames[n.activePlayer]) > n.maxLetters {
-		n.maxLenght = true
-	} else {
-		n.maxLenght = false
-	}
-
-	// Calculate the time since the last letter or number was pressed
-	var timeSinceKey []int
-	for key := ebiten.KeyA; key <= ebiten.KeyZ; key++ {
-		timeSinceKey = append(timeSinceKey, inpututil.KeyPressDuration(key))
-	}
-	var timeSinceNumber []int
-	for key := ebiten.Key0; key <= ebiten.Key9; key++ {
-		timeSinceNumber = append(timeSinceNumber, inpututil.KeyPressDuration(key))
-	}
-
-	// Backspace
-	if len(n.playerNames[n.activePlayer]) > 0 {
-		if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
-			n.playerNames[n.activePlayer] = n.playerNames[n.activePlayer][:len(n.playerNames[n.activePlayer])-1]
-			n.lastMoveTime = time.Now()
-
-		} else if inpututil.KeyPressDuration(ebiten.KeyBackspace) > 30 && time.Since(n.lastMoveTime) >= moveInterval {
-			n.playerNames[n.activePlayer] = n.playerNames[n.activePlayer][:len(n.playerNames[n.activePlayer])-1]
-			n.lastMoveTime = time.Now()
-		}
-	}
-
-	if !n.maxLenght {
-		// Alphabetical characters
-		for key := ebiten.KeyA; key <= ebiten.KeyZ; key++ {
-			if inpututil.IsKeyJustPressed(key) {
-				n.playerNames[n.activePlayer] += string('A' + (key - ebiten.KeyA))
-				n.lastMoveTime = time.Now()
-			} else if timeSinceKey[int(key)] > 30 && time.Since(n.lastMoveTime) >= moveInterval {
-				n.playerNames[n.activePlayer] += string('A' + (key - ebiten.KeyA))
-				n.lastMoveTime = time.Now()
-			}
-		}
-		// Numerical characters
-		for key := ebiten.Key0; key <= ebiten.Key9; key++ {
-			if inpututil.IsKeyJustPressed(key) {
-				n.playerNames[n.activePlayer] += string('0' + (key - ebiten.Key0))
-				n.lastMoveTime = time.Now()
-			} else if timeSinceNumber[int(key)-43] > 30 && time.Since(n.lastMoveTime) >= moveInterval {
-				n.playerNames[n.activePlayer] += string('0' + (key - ebiten.Key0))
-				n.lastMoveTime = time.Now()
-			}
-		}
-	}
+	// Check if max letters has been reached to print the error message
+	n.maxLenght = len(n.playerNames[n.activePlayer]) > n.maxLetters
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
 		n.activePlayer++
