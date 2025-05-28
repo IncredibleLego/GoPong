240b328bd5191be46a2404ee1c562cab3a82f15f
IncredibleLego <ec.francesco@gmail.com>
2025-05-29 00:34:37 +0200

    improved Restart function with delay and log error messages

--- utils/restart.go
+++ utils/restart.go
@@ -1,14 +1,23 @@
 package utils
 
 import (
+	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
 // RestartGame reloads the current process with the same arguments.
 func RestartGame() {
 	exe, err := os.Executable()
 	if err != nil {
+		log.Printf("Error in finding the executable: %v", err)
+		os.Exit(1)
+	}
+	// Warning if the executable is in a temporary directory
+	if strings.Contains(exe, os.TempDir()) {
+		log.Printf("Restart not supported with 'go run'. Compile first with 'go build'")
 		os.Exit(1)
 	}
 	args := os.Args
@@ -22,8 +31,12 @@ func RestartGame() {
 	// Start the new process
 	err = cmd.Start()
 	if err != nil {
+		log.Printf("Error in starting the new process: %v", err)
 		os.Exit(1)
 	}
-	// Terminate the current process
+
+	// Attend briefly to ensure the new process starts before exiting the old one
+	time.Sleep(300 * time.Millisecond)
+
 	os.Exit(0)
 }
