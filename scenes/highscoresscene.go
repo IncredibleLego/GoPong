e52ac4353ab3999e1e2c97cd5cf06c4e5961f98a
IncredibleLego <ec.francesco@gmail.com>
2025-06-06 18:59:12 +0200

    highscoresscene now has a menu and can switch bewteen different sections of the same scene

--- scenes/highscoresscene.go
+++ scenes/highscoresscene.go
@@ -1,13 +1,18 @@
 package scenes
 
 import (
+	"goPong/menu"
 	"goPong/utils"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type HighScoresScene struct {
+	chooseMenu        *menu.RegularMenu
+	actionExecuted    bool
+	highscoreSelected int // 0 for menu, 1 for solo, 2 for computer, 3 for multiplayer
 }
 
 func (h *HighScoresScene) ShouldPreserveState(reason SceneChangeReason) bool {
@@ -15,17 +20,37 @@ func (h *HighScoresScene) ShouldPreserveState(reason SceneChangeReason) bool {
 }
 
 func NewHighScoresScene() *HighScoresScene {
-	return &HighScoresScene{}
+	return &HighScoresScene{
+		chooseMenu:        nil,
+		highscoreSelected: 0,
+	}
 }
 
 func (h *HighScoresScene) Draw(screen *ebiten.Image) {
 
-	utils.ScreenDraw(5, 100, 100, "white", screen, "High Scores")
-
+	switch h.highscoreSelected {
+	case 0:
+		h.chooseMenu.Draw(screen)
+	case 1:
+		utils.ScreenDraw(5, 100, 100, "white", screen, "Solo Mode High Scores")
+	case 2:
+		utils.ScreenDraw(5, 100, 100, "white", screen, "Computer Mode High Scores")
+	case 3:
+		utils.ScreenDraw(5, 100, 100, "white", screen, "Multiplayer Mode High Scores")
+	}
 }
 
 func (h *HighScoresScene) FirstLoad() {
-
+	h.chooseMenu = &menu.RegularMenu{
+		Options: []menu.MenuOption{
+			{Label: "SOLO MODE HIGHSCORES"},
+			{Label: "COMPUTER MODE HIGHSCORES"},
+			{Label: "MULTIPLAYER MODE HIGHSCORES"},
+			{Label: "BACK"},
+		},
+		Selected:     0,
+		LastMoveTime: time.Now(),
+	}
 }
 
 func (h *HighScoresScene) OnEnter() {}
@@ -34,11 +59,52 @@ func (h *HighScoresScene) OnExit() {}
 
 func (h *HighScoresScene) Update() SceneId {
 
-	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
-		return StartSceneId
+	switch h.highscoreSelected {
+	case 0:
+		h.chooseMenu.Update()
+
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) && !h.actionExecuted {
+			id := h.handleMenuSelection()
+			h.actionExecuted = true
+			if id != PauseSceneId {
+				return id
+			}
+		}
+
+		if inpututil.KeyPressDuration(ebiten.KeyEnter) == 0 && !ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			h.actionExecuted = false
+		}
+
+	case 1:
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+			h.highscoreSelected = 0
+		}
+	case 2:
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+			h.highscoreSelected = 0
+		}
+	case 3:
+		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+			h.highscoreSelected = 0
+		}
 	}
 
 	return HighScoresSceneId
 }
 
 var _ Scene = (*HighScoresScene)(nil)
+
+func (h *HighScoresScene) handleMenuSelection() SceneId {
+	switch h.chooseMenu.Selected {
+	case 0:
+		h.highscoreSelected = 1
+	case 1:
+		h.highscoreSelected = 2
+	case 2:
+		h.highscoreSelected = 3
+	case 3:
+		return StartSceneId
+	}
+
+	return HighScoresSceneId
+}
